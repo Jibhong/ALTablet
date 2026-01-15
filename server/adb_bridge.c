0045405e81358a71144edd136fabf78cebddc148
jibhong <pisitjibhong@gmail.com>
2026-01-16 00:07:59 +0700

    input position now working

---
+++ server/adb_bridge.c
@@ -0,0 +1,59 @@
+#include "adb_bridge.h"
+
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <string.h>
+
+#include <unistd.h>
+
+#include <arpa/inet.h>
+
+int adb_bridge_init(const char * host, int port) {
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0) return -1;
+
+    struct sockaddr_in server_addr;
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(port);
+    inet_pton(AF_INET, host, & server_addr.sin_addr);
+
+    if (connect(sock, (struct sockaddr * ) & server_addr, sizeof(server_addr)) < 0) {
+        close(sock);
+        return -1;
+    }
+    return sock;
+
+}
+
+int adb_bridge_receive(int sock, PenData * out_data) {
+    char buffer[256];
+    int bytes_read = recv(sock, buffer, sizeof(buffer) - 1, 0);
+    if (bytes_read > 0) {
+        buffer[bytes_read] = '\0'; // Null-terminate to print as string
+        printf("Received: %s", buffer);
+    } else if (bytes_read == 0) {
+        printf("Connection closed by Android device.\n");
+    }
+    //     buffer[bytes_read] = '\0';
+
+    // Parse the format: "x,y,pressure,isHovering\n"
+    // We use sscanf for easy string-to-float conversion
+    int hover_val;
+    int matched = sscanf(buffer, "%f,%f,%f,%d", &
+        out_data -> x, &
+        out_data -> y, &
+        out_data -> pressure, &
+        hover_val);
+
+    out_data -> is_hovering = hover_val;
+
+//     return (matched == 4);
+    return 1;
+
+}
+
+void adb_bridge_close(int sock) {
+    if (sock >= 0) close(sock);
+}
--- server/altablet.c
+++ server/altablet.c
@@ -5,6 +5,8 @@
 #include <fcntl.h>
 #include <linux/uinput.h>
 
+#include "adb_bridge.h"
+
 void emit(int fd, int type, int code, int val) {
     struct input_event ie = {0};
     ie.type = type;
@@ -58,7 +60,7 @@ int main(void) {
 
     printf("Device created. Check 'libinput list-devices' or 'evtest' now.\n");
     printf("Press ENTER to draw a test line and exit...\n");
-    getchar();
+//     getchar();
 
     // Sequence: Proximity -> Touch -> Move -> Untouch -> Out of Proximity
     emit(fd, EV_KEY, BTN_TOOL_PEN, 1);
@@ -66,7 +68,7 @@ int main(void) {
     usleep(50000);
 
     emit(fd, EV_KEY, BTN_TOUCH, 1);
-    for(int i=0; i<32767; i+=10) {
+    for(int i=0; i<32767; i+=1000) {
         emit(fd, EV_ABS, ABS_X, i);
         emit(fd, EV_ABS, ABS_Y, i);
         emit(fd, EV_ABS, ABS_PRESSURE, 500);
@@ -78,7 +80,45 @@ int main(void) {
     emit(fd, EV_KEY, BTN_TOOL_PEN, 0);
     emit(fd, EV_SYN, SYN_REPORT, 0);
 
-    sleep(1);
+//     sleep(1);
+//     ioctl(fd, UI_DEV_DESTROY);
+//     close(fd);
+
+    int pen_socket = adb_bridge_init("127.0.0.1", 6789);
+    
+    if (pen_socket < 0) {
+        printf("Failed to connect. Is the Android app running and ADB forwarded?\n");
+        return 1;
+    }
+
+    printf("Bridge Connected! Listening for Pen data...\n");
+
+    PenData current_pen;
+    while (1) {
+        if (adb_bridge_receive(pen_socket, &current_pen)) {
+             printf("X: %.2f | Y: %.2f | Pressure: %.2f | Hover: %s\n",
+                   current_pen.x, 
+                   current_pen.y, 
+                   current_pen.pressure,
+                   current_pen.is_hovering ? "YES" : "NO");
+            /*if (!current_pen.is_hovering){
+                emit(fd, EV_KEY, BTN_TOOL_PEN, 1);
+                emit(fd, EV_SYN, SYN_REPORT, 0);
+            }*/
+            emit(fd, EV_ABS, ABS_X, current_pen.x);
+            emit(fd, EV_ABS, ABS_Y, current_pen.y);
+            emit(fd, EV_ABS, ABS_PRESSURE, 500);
+            emit(fd, EV_SYN, SYN_REPORT, 0);
+
+            //emit(fd, EV_KEY, BTN_TOOL_PEN, 0);
+            //emit(fd, EV_SYN, SYN_REPORT, 0);
+        } else {
+            printf("Connection lost.\n");
+            break;
+        }
+    }
+
+    adb_bridge_close(pen_socket);
     ioctl(fd, UI_DEV_DESTROY);
     close(fd);
     return 0;
